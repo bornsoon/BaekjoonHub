92091119ea410cee301ee485775d0d0979f82275
bornsoon <hyolim922@hotmail.com>
2024-08-27 01:11:30 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 730.71 ms, Memory: 24.5 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건에 맞게 수열 변환하기 2 - 181881 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881?language=python3) 
 
 ### 성능 요약
 
-메모리: 24.6 MB, 시간: 1330.47 ms
+메모리: 24.5 MB, 시간: 730.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 01:05:12
+2024년 08월 27일 01:11:28
 
 ### 문제 설명
 
--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -1,17 +1,14 @@
 def solution(arr):
-    x = 0
-    while(True):
-        count = 0
+    x = -1
+    flag = True
+    while flag:
+        flag = False
         for i, j in enumerate(arr):
             if j >= 50 and j % 2 == 0:
                 arr[i] /= 2
+                flag = True
             elif j < 50 and j % 2 == 1:
                 arr[i] = arr[i] * 2 + 1
-            if (j < 50 and j % 2 == 0) or (j >= 50 and j % 2 == 1):
-                count += 1
-            else:
-                pass
-        if count == len(arr):
-            break
+                flag = True
         x += 1
     return x
