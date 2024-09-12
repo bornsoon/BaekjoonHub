8ecbd7ad3b37f4b494477e3b0d2599b65ebaeabb
bornsoon <hyolim922@hotmail.com>
2024-09-12 16:00:15 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,6 +1,3 @@
 def solution(array):
     array.sort()
-    if len(array) % 2 == 1:
-        return array[len(array)//2]
-    else:
-        return array[len(array)//2-1]
+    return array[len(array)//2]
