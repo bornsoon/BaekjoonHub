b58ab7c9b009ea3db945c6ac87cdf671e7edb198
bornsoon <hyolim922@hotmail.com>
2024-10-14 12:29:13 +0900

    [level 0] Title: 문자열 묶기, Time: 31.62 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,8 +1,10 @@
 def solution(strArr):
     length = {}
     for i in strArr:
-        if len(i) in length.keys():
-            length[len(i)] += 1
-        else:
-            length[len(i)] = 1
+        # if len(i) in length.keys():
+        #     length[len(i)] += 1
+        # else:
+        #     length[len(i)] = 1
+    # get함수 사용
+        length[len(i)] = length.get(len(i), 0) + 1
     return max(length.values())
