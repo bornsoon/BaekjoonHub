7cb2da7e022908588d21c7277ab131f591fa7fdb
bornsoon <hyolim922@hotmail.com>
2024-10-15 16:26:52 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    
+    for (int i = 0; i < num_list.size(); i++) {
+        for (int j = 0; j < num_list.size() - i - 1; j++){
+            if (num_list[j] > num_list[j+1]) {
+                int temp = num_list[j];
+                num_list[j] = num_list[j+1];
+                num_list[j+1] = temp;
+            };
+        };
+    };
+    
+    for (int i = 5; i < num_list.size(); i++) {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
