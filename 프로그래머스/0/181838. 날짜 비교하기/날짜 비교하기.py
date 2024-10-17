9ceaddca5ac6251feaaae9d4e608f0cc1350b052
bornsoon <hyolim922@hotmail.com>
2024-10-17 09:49:17 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,8 +1,5 @@
 def solution(date1, date2):
-    if date1[0] > date2[0]:
-        return 0
-    elif date1[0] == date2[0] and date1[1] > date2[1]:
-        return 0
-    elif date1[0] == date2[0] and date1[1] == date2[1] and date1[2] >= date2[2]:
-        return 0
-    return 1
+    for i in range(3):
+        if date1[i]<date2[i]:return 1
+        elif date2[i]<date1[i]: return 0
+    return 0
