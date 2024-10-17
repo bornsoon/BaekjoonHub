8987600e78b72096f0993ed34f4110ed9b1ea581
bornsoon <hyolim922@hotmail.com>
2024-10-17 09:54:12 +0900

    Update 날짜 비교하기.py

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,5 +1,12 @@
 def solution(date1, date2):
-    for i in range(3):
-        if date1[i]<date2[i]:return 1
-        elif date2[i]<date1[i]: return 0
+    for i, j in zip(date1, date2):
+        if i < j:
+            return 1
+        elif i > j:
+            return 0
     return 0
+
+
+# 리스트 비교
+# def solution(date1, date2):
+#     return int(date1 < date2)
