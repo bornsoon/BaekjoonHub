cfce5b32f45a7223641401b059e290306c473032
bornsoon <hyolim922@hotmail.com>
2024-11-17 18:33:25 +0900

    [level 0] Title: 소인수분해, Time: 1.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -2,13 +2,13 @@ def solution(n):
     answer = []
     for i in range(2, n):
         if n % i == 0:
-            flag = True
-            for j in answer:
-                if i % j == 0:
-                    flag = False
+            n /= i
+            answer.append(i)
+            while(True):
+                if n % i == 0:
+                    n /= i
+                else:
                     break
-            if flag:
-                answer.append(i)
     if not answer:
         answer = [n]
     return answer
