dd1164c2909ad1c4d7190529c07aa72764196b26
bornsoon <hyolim922@hotmail.com>
2025-01-28 22:56:21 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,13 +1,6 @@
 def solution(dots):
-    x = dots[3][0]
-    y = dots[3][1]
-    
-    for i in range(3):
-        j = (i + 1) % 3
-        k = (i + 2) % 3
-        d1 = (y - dots[i][1]) / (x - dots[i][0])
-        d2 = (dots[j][1] - dots[k][1]) / (dots[j][0] - dots[k][0])
-        
-        if d1 == d2:
-            return 1
-    return 0
+    (x1, y1), (x2, y2), (x3, y3), (x4, y4) = dots
+    answer1 = (y1 - y2) / (x1 - x2) == (y3 - y4) / (x3 - x4)
+    answer2 = (y2 - y3) / (x2 - x3) == (y1 - y4) / (x1 - x4)
+    answer3 = (y1 - y3) / (x1 - x3) == (y2 - y4) / (x2 - x4)
+    return int(answer1 or answer2 or answer3)
