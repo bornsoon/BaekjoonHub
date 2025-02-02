5bf37ef42d0320a524f85ca00daa21b96be8e029
bornsoon <hyolim922@hotmail.com>
2025-02-02 13:19:23 +0900

    [level 0] Title: 특이한 정렬, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -0,0 +1,19 @@
+def solution(numlist, n):
+    num = {}
+    for i in numlist:
+        num[i] = abs(i-n)
+        
+    sorted_num = sorted(num.items(), key=lambda x: x[1])
+    num_v = sorted(num.values())
+    answer = []
+    for i in range(len(sorted_num)):
+        if sorted_num[i][0] not in answer:
+            if num_v.count(sorted_num[i][1]) > 1:
+                answer.append(max(sorted_num[i + 1][0], sorted_num[i][0]))
+                answer.append(min(sorted_num[i + 1][0], sorted_num[i][0]))
+            else:
+                answer.append(sorted_num[i][0])
+        else:
+            pass
+        
+    return answer
