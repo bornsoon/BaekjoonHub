cea0ed65d54fdb7c2fe6ac5115c46bba08df0180
bornsoon <hyolim922@hotmail.com>
2025-02-06 12:25:54 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -16,3 +16,9 @@ def solution(lines):
         if i > 1:
             answer += 1
     return answer
+
+'''
+def solution(lines):
+    sets = [set(range(start, end)) for start, end in lines]
+    return len(sets[0] & sets[1] | sets[1] & sets[2] | sets[0] & sets[2])
+'''
