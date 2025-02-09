fabe8bc6687f4ed937d1f26e6c5ea9b8302e1e99
bornsoon <hyolim922@hotmail.com>
2025-02-10 02:21:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -6,3 +6,10 @@ def solution(id_pw, db):
             else:
                 return "wrong pw"
     return "fail"
+
+'''
+def solution(id_pw, db):
+    if db_pw := dict(db).get(id_pw[0]):
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"
+'''
